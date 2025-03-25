b9ea455cf56ef2703c812554568b0b2dde3df99a
Thomas Weyermann <thomas.weyermann@inovit.ch>
2025-03-25 11:45:47 +0100

    Add operation validation for suspend and resume

--- suspend_or_resume_fabric_capacity.ps1
+++ suspend_or_resume_fabric_capacity.ps1
@@ -1,5 +1,6 @@
 Param(
     [string]$ResourceID, # e.g. "/subscriptions/12345678-1234-1234-1234-123a12b12d1c/resourceGroups/fabric-rg/providers/Microsoft.Fabric/capacities/myf2capacity"
+    [ValidateSet("suspend", "resume")]
     [string]$operation # "suspend" or "resume"
 )
 
@@ -7,25 +8,44 @@ Param(
 #$operation = "suspend" 
 #$operation = "resume"
 
-Connect-AzAccount -Identity
+$ErrorActionPreference = "Stop"
 
-$tokenObject = Get-AzAccessToken -ResourceUrl "https://management.azure.com/"
-$token = $tokenObject.Token
+try {
+    "Logging in to Azure..."
+    Connect-AzAccount -Identity
+}
+catch {
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+}
 
 $azContext = Get-AzContext
 $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
 $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
 $tokenObject = $profileClient.AcquireAccessToken($azContext.Subscription.TenantId)
 $token = $tokenObject.AccessToken
-
-$url = "https://management.azure.com$ResourceID/$operation" + "?api-version=2022-07-01-preview"
-Write-Output $url
-
 $headers = @{
-    'Content-Type' = 'application/json'
+    'Content-Type'  = 'application/json'
     'Authorization' = "Bearer $token"
 }
 
-$response = Invoke-RestMethod -Uri $url -Method Post -Headers $headers
+$capacityUrl = "https://management.azure.com$ResourceID" + "?api-version=2023-11-01"
+$capacityResourceResponse = Invoke-RestMethod -Uri $capacityUrl -Method Get -Headers $headers
 
+if ($operation -eq "suspend" -and $capacityResourceResponse.properties.state -ne "Active") {
+    Write-Warning "Cannot suspend. Current state: $($capacityResourceResponse.properties.state)"
+    return
+}
+if ($operation -eq "resume" -and $capacityResourceResponse.properties.state -ne "Suspended") {
+    Write-Warning "Cannot resume. Current state: $($capacityResourceResponse.properties.state)"
+    return
+}
+
+$capacityOperationActionUrl = "https://management.azure.com$ResourceID/$operation" + "?api-version=2023-11-01"
+Write-Output $capacityOperationActionUrl
+
+$response = Invoke-RestMethod -Uri $capacityOperationActionUrl -Method Post -Headers $headers
 $response
+if ($response.status -ne "Succeeded") {    
+    throw "Capacity operation did not succeed."
+}
